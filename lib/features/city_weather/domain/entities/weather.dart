fe656dddf8d6ed877e87be61de7c316bb857371b
arkanadli <uzzomaki@gmail.com>
2023-11-30 21:45:25 +0700

    testing usecases 1 success

--- lib/features/city_weather/domain/entities/weather.dart
+++ lib/features/city_weather/domain/entities/weather.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:equatable/equatable.dart';
 
 class WeatherEntity extends Equatable {
--- test/features/city_weather/helpers/test_helper.dart
+++ test/features/city_weather/helpers/test_helper.dart
@@ -1,12 +1,12 @@
 import 'package:city_weather/features/city_weather/domain/repositories/weather_repository.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
 
 @GenerateMocks(
-  [
-    WeatherRepository
-  ]
+  [WeatherRepository],
+  customMocks: [
+    MockSpec<http.Client>(as: #MockHttpClient),
+  ],
 )
-
-void main() {
-  
-}
+void main() {}
